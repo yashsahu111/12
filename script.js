21a3848553159ad84f7926fcc3d9a4f7625cc816
yashsahu111 <ys040589@gmail.com>
2025-08-19 16:11:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const envelope = document.getElementById('envelope');
 
 envelope.addEventListener('click', () => {
   envelope.classList.toggle('open');
+  document.body.classList.toggle('noscroll', envelope.classList.contains('open'));
 });
